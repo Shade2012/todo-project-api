597c7a1af809848af2e55c585689f019f5ac7d1b
Shade2012 <ubasanino@gmail.com>
2024-12-21 08:06:11 +0700

    feature:[S2/Damar] delete todo

--- src/api/router.rs
+++ src/api/router.rs
@@ -1,12 +1,12 @@
 use std::sync::Arc;
 
 use axum::{
-    routing::{get, post},
+    routing::{delete, get, post},
     Router,
 };
 use sqlx::MySqlPool;
 
-use crate::application::{commands::todo_commands::{create_todo_command::create_todo_command, update_todo_command::update_todo_command}
+use crate::application::{commands::todo_commands::{create_todo_command::create_todo_command, delete_todo_command::delete_todo_command, update_todo_command::update_todo_command}
 , queries::todo_queries::{all_todo_query::todo_list_all_query, detail_todo_query::todo_detail_query}};
 
 use super::health_checker_handler;
@@ -22,5 +22,6 @@ pub fn create_router(state: Arc<AppState>) -> Router {
     .route("/api/todos/all", get(todo_list_all_query))
     .route("/api/todos/detail", get(todo_detail_query))
     .route("/api/todos/:id", post(update_todo_command))
+    .route("/api/todos/:id", delete(delete_todo_command))
     .with_state(state)
 }
---
+++ src/application/commands/todo_commands/delete_todo_command.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use axum::{extract::{Path, State}, http::StatusCode, response::IntoResponse, Json};
+use serde_json::json;
+
+use crate::{api::router::AppState, domain::models::todo::todo_to_response};
+
+
+pub async fn delete_todo_command (
+    Path(id): Path<u64>,
+    State(data): State<Arc<AppState>>,
+)
+-> Result<impl IntoResponse,(StatusCode,Json<serde_json::Value>)>
+{
+    let query_result = sqlx::query_as!(Todo,r#"DELETE FROM todos WHERE id = ?"#,&id)
+    .execute(&data.db)
+    .await;
+    match query_result{
+        Ok(todo) => {
+            if todo.rows_affected() > 0 {
+                let response = serde_json::json!({
+                    "status": "success",
+                    "message": format!("Todo with ID: {} successfully deleted", id)
+                });
+                Ok(Json(response))
+            } else {
+                let error_response = serde_json::json!({
+                    "status": "error",
+                    "message": format!("Todo with ID: {} not found", id)
+                });
+                Err((StatusCode::NOT_FOUND, Json(error_response)))
+            }
+        },
+        Err(sqlx::Error::RowNotFound) => {
+            let error_response = serde_json::json!({
+                "status":"error not found",
+                "message":format!("Todo with ID : {} Not Found",&id)
+            });
+            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        }
+        Err(e) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({
+                    "status":"error",
+                    "message":format!("{:?}",e)
+                })),
+            ));
+        }
+    }
+}
